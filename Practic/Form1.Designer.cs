b7d9cd9b9d28eaf9a12721b9654a0f3a8921d2e7
Chebu <Chebu@DESKTOP-3NTP4SI>
2023-10-29 05:16:15 +0200

    Add project files.

---
+++ Practic/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Practic
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            Xn = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            dataGridView1 = new DataGridView();
+            X = new DataGridViewTextBoxColumn();
+            Y = new DataGridViewTextBoxColumn();
+            chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(39, 23);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(74, 23);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "-1,25";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(145, 23);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(74, 23);
+            textBox2.TabIndex = 0;
+            textBox2.Text = "9,39";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(245, 23);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(74, 23);
+            textBox3.TabIndex = 0;
+            textBox3.Text = "0,4";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(344, 23);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(74, 23);
+            textBox4.TabIndex = 0;
+            textBox4.Text = "0,8";
+            // 
+            // Xn
+            // 
+            Xn.AutoSize = true;
+            Xn.Location = new Point(12, 26);
+            Xn.Name = "Xn";
+            Xn.Size = new Size(21, 15);
+            Xn.TabIndex = 1;
+            Xn.Text = "Xn";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(119, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(20, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Xk";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(225, 26);
+            label2.Name = "label2";
+            label2.Size = new Size(14, 15);
+            label2.TabIndex = 1;
+            label2.Text = "h";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(325, 26);
+            label3.Name = "label3";
+            label3.Size = new Size(13, 15);
+            label3.TabIndex = 1;
+            label3.Text = "a";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { X, Y });
+            dataGridView1.Location = new Point(39, 52);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(280, 386);
+            dataGridView1.TabIndex = 2;
+            // 
+            // X
+            // 
+            X.HeaderText = "X";
+            X.Name = "X";
+            // 
+            // Y
+            // 
+            Y.HeaderText = "Y";
+            Y.Name = "Y";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            chart1.ChartAreas.Add(chartArea1);
+            chart1.Location = new Point(344, 52);
+            chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Name = "Series1";
+            chart1.Series.Add(series1);
+            chart1.Size = new Size(425, 386);
+            chart1.TabIndex = 3;
+            chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(440, 23);
+            button1.Name = "button1";
+            button1.Size = new Size(114, 23);
+            button1.TabIndex = 4;
+            button1.Text = " Сalculate";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(chart1);
+            Controls.Add(dataGridView1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(Xn);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)chart1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label Xn;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private DataGridViewTextBoxColumn X;
+        private DataGridViewTextBoxColumn Y;
+        private Button button1;
+    }
+}
